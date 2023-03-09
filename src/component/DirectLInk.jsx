d640e723402a841252afdf7e3b365a35137b9eef
Mahfuz Anam <94157542+mahfuz68@users.noreply.github.com>
2023-03-09 14:02:22 +0600

    Update DirectLInk.jsx

--- src/component/DirectLInk.jsx
+++ src/component/DirectLInk.jsx
@@ -20,7 +20,6 @@ export default function DirectLInk() {
   useEffect(() => {
     const directLink = () => {
       const fileID = inputLink.split("/")[5];
-      const api = "AIzaSyDVyzmm9lL3IE08vAxwio2ubLr2EVf1ucA";
       const outputHLink = `https://www.googleapis.com/drive/v3/files/${fileID}?alt=media&key=${import.meta.env.VITE_API}`;
       setOutputLink(outputHLink);
       return fileID;
